dfc8930597ace620a857646d846ffb9a08ddaecc
Felipe Lopes <felpscokinho@gmail.com>
2025-06-07 11:50:30 -0300

    docs: add Swagger documentation for all product endpoints

---
+++ src/main/java/com/estudo/primeiro/crud/config/swaggerConfig.java
@@ -0,0 +1,18 @@
+package com.estudo.primeiro.crud.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class swaggerConfig {
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("API de produtos")
+                .version("1.0.0")
+                .description("API para administrar os produtos no sistema"));
+    }
+
+}
--- src/main/java/com/estudo/primeiro/crud/controllers/ProductController.java
+++ src/main/java/com/estudo/primeiro/crud/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.estudo.primeiro.crud.controllers;
 import com.estudo.primeiro.crud.DTOs.ProductDTO;
 import com.estudo.primeiro.crud.entities.Product;
 import com.estudo.primeiro.crud.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -48,16 +51,34 @@ public class ProductController {
         this.service = service;
     }
 
+    @Operation(summary = "Listar todos os produtos", description = "Retorna uma lista contendo todos os produtos no sistema.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso!"),
+        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping
     public List<ProductDTO> getAllproducts() {
         return service.findAll().stream().map(this::mapToDTO).toList();
     }
 
+    @Operation(summary = "Buscar produto por ID", description = "Retorna o produto com o ID selecionado")
+    @ApiResponses({
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado"),
+            @ApiResponse(responseCode = "200", description = "Produto encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
         Product product = service.findById(id);
         return product != null ? ResponseEntity.ok(mapToDTO(product)) : ResponseEntity.notFound().build();
     }
+
+    @Operation(summary = "Criar um produto no sistema", description = "Cria um produto baseado nas informações enviadas ao sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Produto criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Falha na criação do produto"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
 
     @PostMapping
     public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO dto) {
@@ -66,7 +87,12 @@ public class ProductController {
         ProductDTO responseDto = mapToDTO(saved);
         return ResponseEntity.ok(responseDto);
     }
-
+    @Operation(summary = "Editar um produto existente", description = "Edita um produto existente, baseando-se no seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Produto editado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Falha na edição do produto"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         Product existing = service.findById(id);
@@ -79,7 +105,12 @@ public class ProductController {
         Product updated = service.save(existing);
         return ResponseEntity.ok(mapToDTO(updated));
     }
-
+    @Operation(summary = "Deletar um produto", description = "Deleta um produto do sistema, baseando-se no seu id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Produto deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         Product existing = service.findById(id);
